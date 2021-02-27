342035b674770aed7bd76707e20782a1e5cced0f
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-02-27 13:40:43 +0100

    update ecosystem

--- ecosystem.js
+++ ecosystem.js
@@ -1,9 +1,25 @@
 module.exports = {
     apps: [
       {
-        name: "WEB",
-        script: "./node_modules/@vue/cli-service/bin/vue-cli-service.js",
-        args: "serve"
-      }
-    ]
-  };
+        name: 'vuetify-dev',
+        script: 'yarn.js',
+        cwd: './packages/vuetify/',
+        args: 'dev',
+      },
+      {
+        name: 'vuetify-build',
+        script: 'yarn.js',
+        cwd: './packages/vuetify/',
+        args: 'watch',
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+      {
+        name: 'vuetify-docs',
+        script: 'yarn.js',
+        cwd: './packages/docs/',
+        args: 'dev',
+      },
+    ],
+  }
